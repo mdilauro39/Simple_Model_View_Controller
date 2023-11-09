9c240bd9240929492bb57ec1dfbe73c84ad90b00
agustin <isfdyt210@gmail.com>
2023-11-08 21:36:18 -0300

    Lo sigo en casa

--- Controller.py
+++ Controller.py
@@ -3,6 +3,32 @@ from person import Persona
 from person_view import PersonaView
 from db_connection import DBConn
 
+#https://guia-tkinter.readthedocs.io/es/develop/chapters/5-basic/5.2-Clases.html
+
+from tkinter import *
+
+
+from six.moves import tkinter as tk
+
+class UI(tk.Frame):
+    """Docstring."""
+
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        """Aqui colocariamos los widgets."""
+        self.parent.title("Un titulo para la ventana")
+
+if __name__ == "__main__":
+    ROOT = tk.Tk()
+    ROOT.geometry("800x600")
+    APP = UI(parent=ROOT)
+    APP.mainloop()
+    ROOT.destroy()
+
 class PersonaController:
 
     def __init__(self):
@@ -71,5 +97,10 @@ class PersonaController:
         self.persona_controller()
 
 
-controller = PersonaController()
+#controller = PersonaController()
 
+
+mvc = Tk()
+mvc.title("Carga de productos")
+mvc.geometry("750x750")
+mvc.mainloop()
