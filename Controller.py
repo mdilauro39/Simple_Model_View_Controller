9e9f6ac576978f71f56991b37e6427c26712a63d
Rodrigo Zelaya <rodrigozelaya2529@gmail.com>
2023-11-14 21:19:50 -0300

    Terminado

--- Controller.py
+++ Controller.py
@@ -6,28 +6,48 @@ from db_connection import DBConn
 #https://guia-tkinter.readthedocs.io/es/develop/chapters/5-basic/5.2-Clases.html
 
 from tkinter import *
+import tkinter as tk
+from tkinter import scrolledtext
+
+class MVC(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.controller = PersonaController()
+        self.persona_view = PersonaView()
+        self.output_text = scrolledtext.ScrolledText(self, wrap=tk.WORD, height=10, width=50)
+        self.output_text.pack()
+        self.button = tk.Button(self, text="Mostrar Menú", command=self.mostrar_menu_en_tkinter)
+        self.button.pack()
+      
+
+    def mostrar_menu_en_tkinter(self):
+        self.output_text.delete('1.0', tk.END)
+        menu_text = self.persona_view.mostrar_menu()
+        self.output_text.insert(tk.END, menu_text)
 
 
-from six.moves import tkinter as tk
 
-class UI(tk.Frame):
-    """Docstring."""
+#from six.moves import tkinter as tk
 
-    def __init__(self, parent=None):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
-        self.init_ui()
+# class UI(tk.Frame):
+#     """Docstring."""
 
-    def init_ui(self):
-        """Aqui colocariamos los widgets."""
-        self.parent.title("Un titulo para la ventana")
+#     def __init__(self, parent=None):
+#         tk.Frame.__init__(self, parent)
+#         self.parent = parent
+#         self.init_ui()
 
-if __name__ == "__main__":
-    ROOT = tk.Tk()
-    ROOT.geometry("800x600")
-    APP = UI(parent=ROOT)
-    APP.mainloop()
-    ROOT.destroy()
+#     def init_ui(self):
+#         """Aqui colocariamos los widgets."""
+#         self.parent.title("Un titulo para la ventana")
+
+# if __name__ == "__main__":
+#     ROOT = tk.Tk()
+#     ROOT.geometry("800x600")
+#     APP = UI(parent=ROOT)
+#     APP.mainloop()
+#     ROOT.destroy()
 
 class PersonaController:
 
@@ -51,14 +71,24 @@ class PersonaController:
 
     def crear_persona_controller(self):
         """Controlador para creación de nueva persona"""
-        (persona_nombre, persona_apellido, persona_dni) = self.vista.crear_persona()
-        persona = Persona()
-        persona.nombre = persona_nombre
-        persona.apellido = persona_apellido
-        persona.dni = persona_dni
-        persona.create()
-        self.vista.confirmar_creacion()
-        self.persona_controller()
+        datos_persona = self.vista.crear_persona()
+
+    
+        if datos_persona:
+        
+            persona_nombre = datos_persona['nombre']
+            persona_apellido = datos_persona['apellido']
+            persona_dni = int(datos_persona['dni'])
+            persona = Persona()
+            persona.nombre = persona_nombre
+            persona.apellido = persona_apellido
+            persona.dni = persona_dni
+            persona.create()
+
+       
+            self.persona_controller()
+
+    
 
     def traer_persona(self):
         """Trae una lista de todos las personas"""
@@ -99,8 +129,12 @@ class PersonaController:
 
 #controller = PersonaController()
 
+if __name__ == "__main__":
+     app = MVC()
+     app.mainloop()
 
-mvc = Tk()
-mvc.title("Carga de productos")
-mvc.geometry("750x750")
-mvc.mainloop()
+
+# mvc = Tk()
+# mvc.title("Carga de productos")
+# mvc.geometry("750x750")
+# mvc.mainloop()
--- person_view.py
+++ person_view.py
@@ -1,98 +1,132 @@
-# -*- coding: utf-8 *-*
-
+import tkinter as tk
+from tkinter import messagebox
 
 class PersonaView:
-
     def __init__(self):
         self.tab1 = "    "
         self.tab2 = "    " * 2
         self.tab3 = "    " * 3
+
+        self.opcion_var = tk.StringVar()
+        self.nombre_var = tk.StringVar()
+        self.apellido_var = tk.StringVar()
+        self.idpersona_var = tk.StringVar()
+        self.dni_var = tk.StringVar()
+
         self.txt_opt = "%sElija una opción: " % self.tab2
         self.txt_nombre = "%sNombre: " % self.tab3
         self.txt_apellido = "%sApellido: " % self.tab3
         self.txt_idpersona = "%sID de Persona: " % self.tab3
-        self.txt_dni = "%sD.N.I de la persona: " % self.tab3
-        pass
+        self.txt_dni = "%sD.N.I: " % self.tab3
 
     def mostrar_menu(self):
         """Vista del menú de opciones"""
-
         menu = """
         Menú del Gestor de Personas
             (1) Crear una persona
             (2) Ver listado de personas
-            (3) Editar una personas
+            (3) Editar una persona
             (4) Eliminar una persona
 
             (0) Salir
-
         """
-        print(menu)
+        messagebox.showinfo("Menú", menu)
 
-        opcion = input(self.txt_opt)
-        return opcion
+       
+        window = tk.Toplevel()
+        tk.Label(window, text=self.txt_opt).pack()
+        tk.Entry(window, textvariable=self.opcion_var).pack()
+        tk.Button(window, text="Confirmar", command=window.destroy).pack()
 
+        window.wait_window(window)
+
+        return self.opcion_var.get().strip()
+    
     def crear_persona(self):
         """Vista del formulario para crear una nueva persona"""
+        self.reset_variables()
 
-        print("""
-        CREAR UN NUEVA  PERSONA
-        """)
-        nombre = input(self.txt_nombre)
-        apellido = input(self.txt_apellido)
-        dni = int(input(self.txt_dni))
-        return (nombre, apellido, dni)
+        window = tk.Toplevel()
+        tk.Label(window, text=self.txt_nombre).pack()
+        tk.Entry(window, textvariable=self.nombre_var).pack()
+        tk.Label(window, text=self.txt_apellido).pack()
+        tk.Entry(window, textvariable=self.apellido_var).pack()
+        tk.Label(window, text=self.txt_dni).pack()
+        tk.Entry(window, textvariable=self.dni_var).pack()
 
-    def confirmar_creacion(self):
+        confirmar_button = tk.Button(window, text="Confirmar", command=lambda: self.confirmar_creacion(window))
+        confirmar_button.pack()
+        window.wait_window(window)
+
+        return {
+            'nombre': self.nombre_var.get(),
+            'apellido': self.apellido_var.get(),
+            'dni': self.dni_var.get()
+        }
+
+    
+
+    def confirmar_creacion(self,window):
         """Vista de confirmación de creación de nueva persona"""
+        messagebox.showinfo("Confirmación", "Persona creada con éxito!")
+        window.destroy()
 
-        print ("""
-        Persona creado con éxito!
-        """)
-
-    def listar_persona(self,listado):
+    def listar_persona(self, listado):
         """Vista para el listado de persona"""
-
-        print ("""
-            LISTADO DE PERSONAS:
-        """)
+        result = "LISTADO DE PERSONAS:\n"
         for row in listado:
-            idpersona = row[0]
-            nombre = row[1]
-            apellido = row[2]
-            dni = row[3]
-            print("%s[%d] %s (%s) %s" % (self.tab3, idpersona, nombre, apellido, dni))
+            idpersona, nombre, apellido, dni = row
+            result += "%s[%d] %s (%s) %s\n" % (self.tab3, idpersona, nombre, apellido, dni)
+        messagebox.showinfo("Listado de Personas", result)
 
     def editar_persona(self, listado):
-        """Vista del formulario para editar un persona"""
+        """Vista del formulario para editar una persona"""
+        self.reset_variables()
 
-        self.listar_persona(listado)
-        print ("\n\n")
-        idpersona = input(self.txt_idpersona)
-        print ("\n")
-        nombre = input(self.txt_nombre)
-        apellido = input(self.txt_apellido)
-        dni = input(self.txt_dni)
-        return (idpersona, nombre, apellido, dni)
+        window = tk.Toplevel()
+        tk.Label(window, text=self.txt_idpersona).pack()
+        tk.Entry(window, textvariable=self.idpersona_var).pack()
+        tk.Label(window, text=self.txt_nombre).pack()
+        tk.Entry(window, textvariable=self.nombre_var).pack()
+        tk.Label(window, text=self.txt_apellido).pack()
+        tk.Entry(window, textvariable=self.apellido_var).pack()
+        tk.Label(window, text=self.txt_dni).pack()
+        tk.Entry(window, textvariable=self.dni_var).pack()
+        tk.Button(window, text="Confirmar", command=window.destroy).pack()
+
+        window.wait_window(window)
+
+        return {
+            'idpersona': self.idpersona_var.get(),
+            'nombre': self.nombre_var.get(),
+            'apellido': self.apellido_var.get(),
+            'dni': self.dni_var.get()
+        }
 
     def confirmar_editar_persona(self):
         """Vista de confirmación de edición"""
-
-        print ("""
-        Persona editada correctamente.
-        """)
+        messagebox.showinfo("Confirmación", "Persona editada correctamente.")
 
     def eliminar_persona(self, listado):
         """Vista de formulario para eliminar una persona"""
+        self.reset_variables()
 
-        self.listar_persona(listado)
-        print ("\n\n")
-        idpersona = input(self.txt_idpersona)
-        print ("\n")
-        return idpersona
+        window = tk.Toplevel()
+        tk.Label(window, text=self.txt_idpersona).pack()
+        tk.Entry(window, textvariable=self.idpersona_var).pack()
+        tk.Button(window, text="Confirmar", command=window.destroy).pack()
+
+        window.wait_window(window)
+
+        return self.idpersona_var.get()
 
     def confirmar_eliminar_persona(self):
-        """Vista de cofirmación eliminar persona"""
-        print( """
-        Persona eliminada correctamente.
-        """)
+        """Vista de confirmación eliminar persona"""
+        messagebox.showinfo("Confirmación", "Persona eliminada correctamente.")
+
+    def reset_variables(self):
+        """Reiniciar variables de instancia"""
+        self.nombre_var.set('')
+        self.apellido_var.set('')
+        self.idpersona_var.set('')
+        self.dni_var.set('')
